f3452cc4e5e9adeebec93e4c4cc3132615bc954a
Manjunath072004 <manjunatha.l@y4elabs.com>
2025-12-16 12:53:30 +0530

    added kpis url

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import MerchantAnalyticsView
+from .views import MerchantKpiView
 
 urlpatterns = [
     path("merchant/", MerchantAnalyticsView.as_view()),
+    path("kpis/", MerchantKpiView.as_view()),
 ]
